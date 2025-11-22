3895d3e2290ff33090c1cc6187d3d90268327f48
srishti chamilall <srishtichamilall14@gmail.com>
2025-11-22 11:34:22 +0400

    Donor profile done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
+  before_action :authenticate_user!, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :phone, :address, :is_donor])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :date_of_birth, :phone, :address, :is_donor])
+    extra_keys = [
+      :first_name, :last_name, :date_of_birth, :phone, :address, :is_donor,
+      { donor_profile_attributes: [:blood_type] }
+    ]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: extra_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: extra_keys)
   end
 end
--- app/models/donor_profile.rb
+++ app/models/donor_profile.rb
@@ -1,19 +1,12 @@
 class DonorProfile < ApplicationRecord
-  belongs_to :user
+  BLOOD_TYPES = %w[A- A+ B- B+ AB- AB+ O- O+].freeze
 
+  belongs_to :user
   has_many :donations, foreign_key: 'donor_id', dependent: :destroy
 
   validates :blood_type, presence: true
   validates :user_id, uniqueness: true
 
-  enum blood_type: {
-    "A-" => 0, "A+" => 1,
-    "AB-" => 2, "AB+" => 3,
-    "B-" => 4, "B+" => 5,
-    "O-" => 6, "O+" => 7
-  }
-
   enum donor_status: { active: 0, inactive: 1 }
-
   enum eligibility_status: { eligible: 0, ineligible: 1 }
 end
